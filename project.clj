d0254c795623f3669db4632f5f55506de57db816
rdsr <rdsr.me@gmail.com>
2010-10-11 16:08:33 +0530

    learning clojure protocols

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-fu "1.0.0-SNAPSHOT"
+ :description "clojure learnings/practices/experiences"
+ :dependencies [[org.clojure/clojure "1.2.0"]
+                [org.clojure/clojure-contrib "1.2.0"]]
+ :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ src/clj_fu/median.clj
@@ -0,0 +1,33 @@
+(ns clj-fu.median)
+
+(defn first-half [v]
+  (subvec v 0 (/ (count v) 2)))
+
+(defn second-half [v]
+  (let [sz (count v)]
+    (if (even? sz)
+      (subvec v (quot sz 2))
+      (subvec v (inc (quot sz 2))))))
+
+(defn median [v]
+  (let [sz (count v)]
+  (if (even? sz)
+    (v (dec (quot sz 2)))
+    (v (quot sz 2)))))
+
+(defn median-2v
+  "finds the median of two sorted vectors"
+  [u v]
+  (let [m (median u)
+        n (median v)]
+    (cond
+     (= m n) m
+     (= (count u) 1) (if (< m n) m n)
+     (< m n) (median-2v (second-half u) (first-half v))
+     (> m n) (median-2v (second-half v) (first-half u)))))
+
+(defn make-vec [n]
+  (into [] (sort (map #(rand-int %) (range 0 n)))))
+
+
+(median-2v (make-vec 1000000) (make-vec 1000000))
